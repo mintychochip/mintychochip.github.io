345c895edad6d80773a948acd9e7f43b3e37edc7
justi <jlo2@csub.edu>
2024-10-20 21:25:58 -0700

    ini

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mintychochip.github.io
-Website
+# prompt
+For your first app, keep it simple. Set up a new project called "Hello" and complete the following:
+1. Create your first view.
+2. The view will contain one button with the text "Hello".
+3. When the "Hello" button is clicked it should launch a new view/model/alert.
+4. The new view/model will have a single text label that says "Hello World" and an OK button.
+5. When the OK button is clicked the application should go back to the first view.
---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { goto } from '$app/navigation'
+    import Button from '$lib/Button.svelte'
+    import { typewriter } from '$lib/typewriter.js'
+</script>
+
+<div class = "container">
+    <div class = "sub-container">
+        <div>
+            <span class = "text" in:typewriter>Hello World!</span>
+            <span class = "text blinking">_</span>
+        </div>
+        <Button on:click={() => {
+            goto('/')
+        }}>Ok</Button>
+    </div>
+</div>
+
+<style>    
+    .text {
+        font-size: 72px;
+    }
+
+    @keyframes blink {
+        50% {opacity: 0}
+    }
+
+    .blinking {
+        animation: blink 0.7s step-end infinite;
+        display: inline;
+    }
+</style>
