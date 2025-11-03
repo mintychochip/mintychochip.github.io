950672a8cade22d32d3ba96ef1d1b579c60e1ed5
mintychochip <jlo2@csub.edu>
2025-11-03 11:14:19 -0800

    kermit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mintychochip.github.io',
-    base: '/mintychochip.github.io',
+  site: 'https://mintychochip.github.io',
+  base: '/mintychochip.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [vue()],
 });
